8ee119cfbd6bd8575ce1a024696fcd40b729aa13
sheetal <singh.sheetal3039@gmail.com>
2019-03-30 18:24:30 +0530

    login signup templates created

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html', {'title': 'HOME'})
+
+def login(request):
+    return render(request, 'login.html', {'title': 'Login'})
+
+def register(request):
+    return render(request, 'register.html', {'title':'Regitser'})
