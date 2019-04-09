bb9744975141a44d72fa35ca758e02e0baa40078
rht6226 <rohitraazanand567@gmail.com>
2019-04-09 10:15:09 +0530

    Quiz Registration created

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from .models import Quiz, Question
+from admin_panel.models import AnswerSheet, Answer
 from datetime import datetime, date
 from django.utils.timezone import datetime, timedelta
 
@@ -21,7 +23,7 @@ def instructions(request,quizid):
         today = datetime.now()
         temp = datetime.combine(date.min, today.time()) - datetime.combine(date.min, item.quiz_time.time())
         print(temp)
-        if (temp < timedelta(minutes=5)):
+        if temp < timedelta(minutes=5):
             print("hola")
             margin = 1
         else:
@@ -33,4 +35,47 @@ def instructions(request,quizid):
     except Quiz.DoesNotExist:
         messages.info(request, 'Quiz does not exists!')
         return redirect('dashboard')
+
+
+@login_required()
+def register_quiz(request, quizid):
+
+    try:
+        # Get Quiz form DB
+        quiz_instance = Quiz.objects.get(quiz_id=quizid)
+        user = request.user
+        tags = quiz_instance.tags
+        tags_list = tags.split(';')
+
+        if request.method == 'POST':
+            try:
+                # Check if user is already registered for the quiz
+                sheet_instance = AnswerSheet.objects.get(quiz=quiz_instance, contestant=user)
+                messages.info(request, 'You have already registered for this quiz')
+                return redirect('dashboard')
+
+            except AnswerSheet.DoesNotExist:
+                sheet_instance = AnswerSheet()
+
+                sheet_instance.quiz = quiz_instance
+                sheet_instance.contestant = user
+                sheet_instance.is_valid = True
+
+                sheet_instance.save()
+                messages.info(request, 'Best of Luck!! You have been successfully registered.')
+                return redirect('dashboard')
+
+        # Get the page
+        else:
+            context = {'title': 'Register for Quiz', 'quiz': quiz_instance, 'user': user, 'tags': tags_list}
+
+        return render(request, 'register_quiz.html', context=context)
+
+    except Quiz.DoesNotExist:
+        # No such quiz found in the database
+        messages.info(request, 'Quiz does not exists')
+        return redirect('dashboard')
+
+
+
 
