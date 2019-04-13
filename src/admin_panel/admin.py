cc09457ba4f8fe36b4ba54a2049b953c26282f7e
rht6226 <rohitraazanand567@gmail.com>
2019-04-13 15:34:52 +0530

    modification

--- src/admin_panel/admin.py
+++ src/admin_panel/admin.py
@@ -20,7 +20,7 @@ class AnswerAdmin(admin.ModelAdmin):
     list_display = ('sheet', 'question', 'marks_awarded', 'is_attempted', 'is_graded')
     list_filter = ('sheet', 'is_attempted', 'is_graded')
     fieldsets = (
-        (None, {'fields': ('sheet', 'question', 'marks_awarded', 'is_attempted')}),
+        (None, {'fields': ('sheet', 'question', 'marks_awarded', 'is_attempted', 'is_graded')}),
         ('Time', {'fields': ('start_time', 'end_time')}),
         ('If Subjective', {'fields': ('subjective_answer', )}),
         ('If Objective', {'fields': ('response_A', 'response_B', 'response_C', 'response_D')}),
